470d7f28b8c94bfe1f44db2bc10742f794e4eda1
Alyssa Ross <hi@alyssa.is>
2014-12-13 14:33:44 +0000

    Remove unused line

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var request = require("request");
 var express = require("express");
 
 var getSongURL = function (pageURL, callback) {
-  var options = ;
   request.get({ uri: "http://api.diffbot.com/v3/analyze", json: true, qs: {
     token: process.env.DIFFBOT_TOKEN,
     url: pageURL,
