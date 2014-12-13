a448c9cd0037115351c9be9dbb0cab20af4b4399
Alyssa Ross <hi@alyssa.is>
2014-12-13 17:15:37 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var getSongURL = function (pageURL, callback) {
     if (error) return callback(error);
     if (!body.hasOwnProperty("entities")) return callback("invalid alchemy response");
     if (body.entities.length < 1) return callback("no entities");
+    console.log(body);
     console.log(body.entities.map(function(x) { return x.name }));
     request.get({ uri: "http://api.musixmatch.com/ws/1.1/track.search", json: true, qs: {
       apikey: process.env.MUSIXMATCH_API_KEY,
