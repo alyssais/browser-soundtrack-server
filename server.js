dfcd6fccb71a183b7af7d81eb64b2d8eb96c9b00
Alyssa Ross <hi@alyssa.is>
2014-12-13 17:05:04 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var getSongURL = function (pageURL, callback) {
     url: pageURL
   } }, function (error, response, body) {
     if (error) return callback(error);
+    console.log(body);
     if (body.entities.length < 1) return callback("no entities");
     console.log(body.entities.map(function(x) { return x.name }));
     request.get({ uri: "http://api.musixmatch.com/ws/1.1/track.search", json: true, qs: {
@@ -15,9 +16,11 @@ var getSongURL = function (pageURL, callback) {
       s_track_rating: "desc"
     } }, function(error, response, body) {
       if (error) return callback(error);
+      console.log(body);
       var tracks = body.message.body.track_list;
       if (tracks.length < 1) return callback("no tracks");
       request({ uri: "https://api.spotify.com/v1/tracks/" + tracks[0].track.track_spotify_id, json: true }, function(error, response, body) {
+        console.log(body);
         if (error) return callback(error);
         callback(null, body.preview_url);
       });
